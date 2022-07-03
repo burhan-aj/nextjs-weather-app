85aa5b9124986e03f1122b35c04ad10fa5962514
Burhanuddin Ajmerwala <burhanuddin.a@thecareerlabs.com>
2022-07-03 18:24:08 +0530

    alt attr

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
         {/* Overlay */}
         <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]'/>
         {/* Background Image */}
-        <Image src={'https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2575&q=80'} layout='fill' className='object-cover' />
+        <Image src={'https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2575&q=80'} alt='/' layout='fill' className='object-cover' />
       
         {/* Search */}
         <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10'>
